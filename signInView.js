3ed714219875f49922b71672af0d8806e40a5852
LinnOestgaard <linn.oes98@gmail.com>
2022-01-18 13:03:35 +0100

    Update signIn & signUp Views

--- signInView.js
+++ signInView.js
@@ -1,19 +1,19 @@
 function signInView() {
     let html = "";
-
+    html += generateLogoHTML();
     html += /*html*/`
-    <div class="logo">My Movie List</div>
-    <br/>
-    <br/>
-    <h1>Sign in</h1>
-    <br/>
-    <input onchange="setUsername(this.value)" type="text" placeholder="Username"/>
-    <br/>
-    <input onchange="setPassword(this.value)" type="password" placeholder="Password"/>
-    <br/>
-    <button class="signUp" onclick="signUp()">Sign Up</button>
-    <button class="signIn" onclick="signIn()">Sign In</button>
+    <div class="signInView">
+        <h1>Sign in</h1>
+        <br/>
+        <input onchange="setUsername(this.value)" type="text" placeholder="Username"/>
+        <br/>
+        <input onchange="setPassword(this.value)" type="password" placeholder="Password"/>
+        <br/>
+        <button class="signUp" onclick="signUp()">Sign Up</button>
+        <button class="signIn" onclick="signIn()">Sign In</button>
+    </div>
     `;
 
+    html += generateNavbarHTML();
     document.getElementById('app').innerHTML = html;
 }
--- signUpView.js
+++ signUpView.js
@@ -3,17 +3,15 @@ function signUpView() {
     html += generateLogoHTML();
     html += /*html*/`
     <div class="signUpView">
-    <br/>
-    <br/>
-    <h1>Sign Up</h1>
-    <br/>
-    <input onchange="setUsername(this.value)" type="text" placeholder="Create username"/>
-    <br/>
-    <input onchange="setPassword(this.value)" type="password" placeholder="Create password"/>
-    <br/>
-    <input onchange="setEmail(this.value)" type="text" placeholder="Email"/>
-    <br/>
-    <button class="signUp" onclick="signUp()">Sign Up</button>
+        <h1>Sign Up</h1>
+        <br/>
+        <input onchange="setUsername(this.value)" type="text" placeholder="Create username"/>
+        <br/>
+        <input onchange="setPassword(this.value)" type="password" placeholder="Create password"/>
+        <br/>
+        <input onchange="setEmail(this.value)" type="text" placeholder="Email"/>
+        <br/>
+        <button class="signUp" onclick="signUp()">Sign Up</button>
     </div>
     `;
 
