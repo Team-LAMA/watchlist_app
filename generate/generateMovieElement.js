5b7e7963e0ecff6e2141772eacc4a1bcb9ebedf4
seedback <seedback2@gmail.com>
2022-01-28 01:31:27 +0100

    fix generateMovieElement

--- generate/generateMovieElement.js
+++ generate/generateMovieElement.js
@@ -17,12 +17,12 @@ function generateMovieElement(movieList, index, onClick, options = {}) {
 		<div class="movieElement container ${options.scroll ? 'scroll' : ''} ${options.expanded ? 'expanded' : ''} ${options.editable ? 'editable' : ''}" onclick="${onClick}" index='${index}'>
 	`
 
-	// Generate the "minimal" or collapsed view of the element
+	// Generate movieElement minimal
 	html += /*html*/ `
 		<div class="movieElement minimal">
 			<img class="movieElement image" src="${movieList.movies[index].image}"/>
 			<div class="movieElement minimal midSection"> 
-				<div class="movieElement movieTitle">
+				<div class="movieElement movieTitle ${options.scroll ? 'scroll' : ''}">
 					<div style="text-align: center">${movieList.movies[index].title}</div>
 				</div>
 				<div class="movieElement releaseYear">
@@ -41,20 +41,24 @@ function generateMovieElement(movieList, index, onClick, options = {}) {
 			</div>
 		</div>
 	`
+	// End of movieElement minimal
 
+	// Generate movieElement expanded
 	html += /*html*/ `
 		<div class="movieElement expanded">
 	`
-
-	if (movieList.movies[index].directors.length > 0 || movieList.movies[index].actors.length > 0) {
+	
+	// Adding <hr> for genre/directors
+	if (movieList.movies[index].genres.length > 0 || movieList.movies[index].directors.length > 0) {
 		html += /*html*/ `<hr/>`
 	}
 
+	// Opening movieElement detailBox
 	html += /*html*/ `
 		<div class="movieElement detailBox ${options.scroll ? 'scroll' : ''}">
 	`
 
-	// Adding in the Genere(s) section
+	// Opening genre list + title
 	if (movieList.movies[index].genres.length > 0) {
 		html += /*html*/ `
 			<div>
@@ -70,8 +74,9 @@ function generateMovieElement(movieList, index, onClick, options = {}) {
 			</div>
 		`
 	}
+	// Closing genre list + title
 
-	// Adding in the Director(s) section
+	// Opening director list + title
 	if (movieList.movies[index].directors.length > 0) {
 		html += /*html*/ `
 			<div>
@@ -87,32 +92,29 @@ function generateMovieElement(movieList, index, onClick, options = {}) {
 			</div>
 		`
 	}
+	// Closing director list + title
 
-	// closing detailBox
+	// Closing movieElement detailBox (genre/director)
 	html += /*html*/ `
 		</div>
 	`
 
+	// Opening  description + title
 	if (movieList.movies[index].description){
 		html += /*html*/ `
 			<hr/>
-			<div class="movieElement description">
-				<div class="movieElement description title">
-					<u>
-						Description
-					</u>
-				</div>
-				<div>${movieList.movies[index].description}<div>
-			</div>
+			<div class="movieElement description title ${options.scroll ? 'scroll' : ''}">Description</div>
+			<div class="movieElement description ${options.scroll ? 'scroll' : ''}">${movieList.movies[index].description}</div>
 		`
 	}
+	// Closing description + title
 
-	// Adding in the Actor(s) section
+	// Opening actors + title
 	if (movieList.movies[index].actors.length > 0) {
 		html += /*html*/ `
 			<hr>
-			<div class="movieElement directors">
-				<u>Director${(movieList.movies[index].actors.length > 1) ? "s" : ""}</u>
+			<div class="movieElement actors">
+				<u>Actor${(movieList.movies[index].actors.length > 1) ? "s" : ""}</u>
 				<div>
 		`
 		html += movieList.movies[index].actors[0];
@@ -124,15 +126,148 @@ function generateMovieElement(movieList, index, onClick, options = {}) {
 			</div>
 		`
 	}
+	// Closing actors + title
 
-	// closing expanded
+	// Opening personal comment
+	if(movieList.personalComments[index]){
+		html += /*html*/ `
+			<hr/>
+			<div class="movieElement personalComments">
+				<u>Personal Comments</u>
+				<div>${movieList.personalComments[index]}</div>
+			</div>
+		`
+	}
+	// Closing personal comment
+
+	// Closing movieElement expanded
 	html += /*html*/ `
 		</div>
 	`
 
+	// Closing movieElement container scroll? expanded? editable?
 	html += /*html*/ `
 		</div>
-	`;
+	`
+
+
+
+	//// Generate the "minimal" or collapsed view of the element
+	//html += /*html*/ `
+	//	<div class="movieElement minimal">
+	//		<img class="movieElement image" src="${movieList.movies[index].image}"/>
+	//		<div class="movieElement minimal midSection"> 
+	//			<div class="movieElement movieTitle">
+	//				<div style="text-align: center">${movieList.movies[index].title}</div>
+	//			</div>
+	//			<div class="movieElement releaseYear">
+	//				<div>${movieList.movies[index].year}</div>
+	//			</div>
+	//		</div>
+	//		<div class="movieElement score ${options.scroll ? 'scroll' : ''}">
+	//			<div style="display: flex;">
+	//				<div style="font-size: 3vh">
+	//					${(!movieList.ratings[index]) ? 0 : movieList.ratings[index]}
+	//				</div>
+	//				<div style="font-size: 2vh; margin-top: 1vh; margin-right: 1vh">
+	//					/10
+	//				</div>
+	//			</div>
+	//		</div>
+	//	</div>
+	//`
+
+	//html += /*html*/ `
+	//	<div class="movieElement expanded">
+	//`
+
+	//if (movieList.movies[index].directors.length > 0 || movieList.movies[index].actors.length > 0) {
+	//	html += /*html*/ `<hr/>`
+	//}
+
+	//html += /*html*/ `
+	//	<div class="movieElement detailBox ${options.scroll ? 'scroll' : ''}">
+	//`
+
+	//// Adding in the Genere(s) section
+	//if (movieList.movies[index].genres.length > 0) {
+	//	html += /*html*/ `
+	//		<div>
+	//			<u>Genre${(movieList.movies[index].genres.length > 1) ? "s" : ""}</u>
+	//			<div>
+	//	`
+	//	html += movieList.movies[index].genres[0];
+	//	for (let i = 1; i < movieList.movies[index].genres.length; i++) {
+	//		html += ", " + movieList.movies[index].genres[i];
+	//	}
+	//	html += /*html*/ `
+	//			</div>
+	//		</div>
+	//	`
+	//}
+
+	//// Adding in the Director(s) section
+	//if (movieList.movies[index].directors.length > 0) {
+	//	html += /*html*/ `
+	//		<div>
+	//			<u>Director${(movieList.movies[index].directors.length > 1) ? "s" : ""}</u>
+	//			<div>
+	//	`
+	//	html += movieList.movies[index].directors[0];
+	//	for (let i = 1; i < movieList.movies[index].directors.length; i++) {
+	//		html += ", " + movieList.movies[index].directors[i];
+	//	}
+	//	html += /*html*/ `
+	//			</div>
+	//		</div>
+	//	`
+	//}
+
+	//// closing detailBox
+	//html += /*html*/ `
+	//	</div>
+	//`
+
+	//if (movieList.movies[index].description){
+	//	html += /*html*/ `
+	//		<hr/>
+	//		<div class="movieElement description">
+	//			<div class="movieElement description title">
+	//				<u>
+	//					Description
+	//				</u>
+	//			</div>
+	//			<div>${movieList.movies[index].description}<div>
+	//		</div>
+	//	`
+	//}
+
+	//// Adding in the Actor(s) section
+	//if (movieList.movies[index].actors.length > 0) {
+	//	html += /*html*/ `
+	//		<hr>
+	//		<div class="movieElement directors">
+	//			<u>Director${(movieList.movies[index].actors.length > 1) ? "s" : ""}</u>
+	//			<div>
+	//	`
+	//	html += movieList.movies[index].actors[0];
+	//	for (let i = 1; i < movieList.movies[index].actors.length; i++) {
+	//		html += ", " + movieList.movies[index].actors[i];
+	//	}
+	//	html += /*html*/ `
+	//			</div>
+	//		</div>
+	//	`
+	//}
+
+	//// closing expanded
+	//html += /*html*/ `
+	//	</div>
+	//`
+
+	//html += /*html*/ `
+	//	</div>
+	//`;
 
 	return html;
 }
