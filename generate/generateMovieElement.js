0d6a4657c513cd0e46e944b2357fb4dcb558abfb
MagnusKristian <92784711+MagnusKristian@users.noreply.github.com>
2022-02-10 15:12:30 +0100

    Revert "temp"
    This reverts commit c85fc14dc53d41ca616c8d10ab2689f27e964dcf.

--- generate/generateMovieElement.js
+++ generate/generateMovieElement.js
@@ -191,7 +191,6 @@ function generateMovieElement(movieList, index, onClick, options = {}) {
 	}
 	// Closing actors + title
 
-	
 	// Opening personal comment
 	if (movieList.personalComments[index] || options.editable) {
 		html += /*html*/ `
