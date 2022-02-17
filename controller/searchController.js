36907407b576e7db8df9e9663822356a791a1c57
seedback <seedback2@gmail.com>
2022-02-17 13:42:03 +0100

    Feat: Improve searching possibilities
    Fix issue #49

--- controller/searchController.js
+++ controller/searchController.js
@@ -16,13 +16,32 @@ function getSearchResults(searchWord = ""){
 	let resultList = [];
 	searchWord = searchWord.toLowerCase();
 
+  // Searches through all users and checking user-specific data to see if the
+  //   list should be added to the list shown after the search
 	for(let i = 0; i < model.users.length; i++){
 		if(model.users[i].profileName.toLowerCase().search(searchWord) != -1
-			|| model.users[i].movieLists[0].name.toLowerCase().search(searchWord) != -1){
+			|| model.users[i].movieLists[0].name.toLowerCase().search(searchWord) != -1
+      || model.users[i].favGenre.toLowerCase().search(searchWord) != -1){
 			resultList.push(model.users[i]);
-		}
+		} else {
+      // Searches through all movies in all user-lists and checking all
+      //   movie-specific data to see if the list should be added to the list
+      //   shown after the search
+      let curUser = model.users[i];
+      let isAdded = false;
+      for(let n = 0; n < curUser.movieLists[0].movies.length && isAdded == false; n++){
+        let curMovie = curUser.movieLists[0].movies[n];          
+        if(curMovie.genres){
+          if(curMovie.genres.toLowerCase().search(searchWord) != -1
+          || curMovie.actors.toLowerCase().search(searchWord) != -1
+          || curMovie.directors.toLowerCase().search(searchWord) != -1){
+            isAdded = true;
+            resultList.push(curUser);
+          }
+        }
+      } 
+    }
 	}
-
 
 	let html = "";
 
