c099b334c0ebb365b25c3ec37b0f63540dd8df11
seedback <seedback2@gmail.com>
2022-02-21 11:40:54 +0100

    Feat: Add autofocus and enter confirm

--- view/signInView.js
+++ view/signInView.js
@@ -5,7 +5,7 @@ function signInView() {
 	html += /*html*/`
     <div class="signIn container">
         <h1>Sign in</h1>
-        <input onchange="updateUsernameSignIn(this)" type="text" placeholder="Username"/>
+        <input onchange="updateUsernameSignIn(this)" type="text" placeholder="Username" autofocus/>
         <input onchange="updatePasswordSignIn(this)" type="password" placeholder="Password"/>
 				<div>
         	<button class="signUpBtn" onclick="go('signUp')">Sign Up</button>
@@ -21,4 +21,31 @@ function signInView() {
 
 	html += generateNavbarHTML();
 	document.getElementById('app').innerHTML = html;
+
+  document.getElementsByClassName("signInBtn")[0].addEventListener("keyup", event => {
+    if(event.keyCode === 13) { // 13 = enter
+      event.preventDefault();
+      document.getElementsByClassName("signInBtn")[0].click();
+    }
+  });
+  document.getElementsByClassName("signUpBtn")[0].addEventListener("keyup", event => {
+    if(event.keyCode === 13) { // 13 = enter
+      event.preventDefault();
+      document.getElementsByClassName("signUpBtn")[0].click();
+    }
+  });
+  document.getElementsByClassName("signIn container")[0].getElementsByTagName("input")[0].addEventListener("keyup", event => {
+    if(event.keyCode === 13) { // 13 = enter
+      event.preventDefault();
+      document.getElementsByClassName("signInBtn")[0].click();
+    }
+  });
+  document.getElementsByClassName("signIn container")[0].getElementsByTagName("input")[1].addEventListener("keyup", event => {
+    if(event.keyCode === 13) { // 13 = enter
+      event.preventDefault();
+      document.getElementsByClassName("signInBtn")[0].click();
+    }
+  });
+
+  document.getElementsByClassName("signInBtn")[0].focus();
 }
--- view/signUpView.js
+++ view/signUpView.js
@@ -1,12 +1,12 @@
 // SignUp view
 
 function signUpView() {
-	let html = "";
-	html += generateLogoHTML();
-	html += /*html*/`
+  let html = "";
+  html += generateLogoHTML();
+  html += /*html*/`
     <div class="signUp container">
 			<h1>Sign Up</h1>
-			<input onchange="updateUsernameSignUp(this)" type="text" placeholder="Create username"/>
+			<input onchange="updateUsernameSignUp(this)" type="text" placeholder="Create username" autofocus/>
 			<input onchange="updatePasswordSignUp(this)" type="password" placeholder="Create password"/>
 			<input onchange="updateEmailSignUp(this)" type="text" placeholder="Email"/>
 			<div>
@@ -16,8 +16,35 @@ function signUpView() {
     </div>
   `;
 
-	html += generateNavbarHTML();
-	document.getElementById('app').innerHTML = html;
+  html += generateNavbarHTML();
+  document.getElementById('app').innerHTML = html;
+
+  document.getElementsByClassName("signUpBtn")[0].addEventListener("keyup", event => {
+    if (event.keyCode === 13) { // 13 = enter
+      event.preventDefault();
+      document.getElementsByClassName("signUpBtn")[0].click();
+    }
+  });
+  document.getElementsByClassName("signUp container")[0].getElementsByTagName("input")[0].addEventListener("keyup", event => {
+    if (event.keyCode === 13) { // 13 = enter
+      event.preventDefault();
+      document.getElementsByClassName("signUpBtn")[0].click();
+    }
+  });
+  document.getElementsByClassName("signUp container")[0].getElementsByTagName("input")[1].addEventListener("keyup", event => {
+    if (event.keyCode === 13) { // 13 = enter
+      event.preventDefault();
+      document.getElementsByClassName("signUpBtn")[0].click();
+    }
+  });
+  document.getElementsByClassName("signUp container")[0].getElementsByTagName("input")[2].addEventListener("keyup", event => {
+    if (event.keyCode === 13) { // 13 = enter
+      event.preventDefault();
+      document.getElementsByClassName("signUpBtn")[0].click();
+    }
+  });
+
+  document.getElementsByClassName("signUpBtn")[0].focus();
 }
 
 
